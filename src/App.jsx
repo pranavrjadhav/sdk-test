aa2c320383ba6f8d2ff5fb66fc5ba135bf6fd02e
Joker010 <pranavitachi9594@gmial.com>
2025-07-09 12:32:01 +0530

    build:- add div in return

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@
 function App() {
 
    console.log("hi there pranav");
-   <div>
+   
+  
+  return (
+    <>
+    <div>
     <h1 class="text-blue-900">
       This is Solid JS
     </h1>
    </div>
-  
-  return (
-    <></>
+    </>
   );
 }
 
